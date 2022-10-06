37a02833ed6be7cbbb5659fd128190dfbfa80693
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-10-05 19:23:03 -0700

    Add files via upload

---
+++ tictactoe.c
@@ -0,0 +1,23 @@
+//Tic Tac Toe game between 2 players or player vs player
+
+
+int main(){
+
+    //prompt user for game they wish to play
+    //write code here
+
+    //read information from console
+    //write code here
+
+    //pvp code
+    //write code here
+
+    //computer code
+    //write code here
+
+    //
+
+
+
+    return 0;
+}
