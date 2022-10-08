ccda29427efed520ced4e732975c975dd6ab739d
adroman1cpp <112600733+adroman1cpp@users.noreply.github.com>
2022-10-07 18:14:48 -0700

    Add files via upload
    added functions

--- tictactoe.c
+++ tictactoe.c
@@ -1,4 +1,19 @@
 //Tic Tac Toe game between 2 players or player vs player
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+//declare functions
+void printGame();
+void setGame();
+char winner;
+int numberOfPlaces;
+void player1;
+void aiComputer;
+void whoWins(char);
+
+
 
 
 int main(){
@@ -21,3 +36,12 @@ int main(){
 
     return 0;
 }
+
+
+void printGame();
+void setGame();
+char winner;
+int numberOfPlaces;
+void player1;
+void aiComputer;
+void whoWins(char);
